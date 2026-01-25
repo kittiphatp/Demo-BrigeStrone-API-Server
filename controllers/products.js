615c06db5025e8fb8f1ff31be387ec6a119ebe39
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2026-01-25 09:35:33 +0700

    update product.js in controller

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,12 @@ const getProductsByCategory = (req, res) => {
     const category = req.params.category;
     const products = require('../data/products.json');
     const filteredProducts = products.filter(product => product.category === category);
-    res.status(200).json(filteredProducts);
+    
+    if (filteredProducts.length === 0) {
+        res.status(404).json({ message: "No products found for this category" });
+    } else {
+        res.status(200).json(filteredProducts);
+    }
 }
 
 module.exports = {
