1821cd51fefd73b667bcfd8625a6ff6d5151b24b
Kittiphat Pisuittiporn <45120803+kittiphatp@users.noreply.github.com>
2026-01-25 19:36:25 +0700

    Add getProductsById function to products controller

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const getProductsByCategory = (req, res) => {
     const category = req.params.category;
     const products = require('../data/products.json');
     const filteredProducts = products.filter(product => product.category === category);
-    
+
     if (filteredProducts.length === 0) {
         res.status(404).json({ message: "No products found for this category" });
     } else {
@@ -10,6 +10,19 @@ const getProductsByCategory = (req, res) => {
     }
 }
 
+const getProductsById = (req, res) => {
+    const id = req.params.id;
+    const products = require('../data/products.json');
+    const product = products.find(product => product.id === parseInt(id));
+
+    if (!product) {
+        res.status(404).json({ message: "Product not found" });
+    } else {
+        res.status(200).json(product);
+    }
+}
+
 module.exports = {
-    getProductsByCategory
+    getProductsByCategory,
+    getProductsById
 }
